0e14330ca33ea5af8235b74c19e3f30d41146207
Spanti Nicola <rydroid_dev@yahoo.com>
2014-10-18 19:01:59 +0200

    Update freecyngn.sh

--- freecyngn.sh
+++ freecyngn.sh
@@ -4,7 +4,7 @@ export BASE_DIR=/system/freecyngn
 export TMP_DIR=/tmp/freecyngn
 LOGFILE=$BASE_DIR/log
 
-echo -n "" > $LOGFILE
+echo -n '' > $LOGFILE
 chmod 644 $LOGFILE
 
 export PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/sbin
@@ -33,47 +33,46 @@ fi
 
 if ! [ -f $SETTINGS_APP ]
 then
-	echo "Did not find Settings.apk on /system, is any ROM installed?" >> $LOGFILE
+	echo 'Did not find Settings.apk on /system, is any ROM installed?' >> $LOGFILE
 	exit
 fi
 
-echo "Creating directory structure..." >> $LOGFILE
+echo 'Creating directory structure...' >> $LOGFILE
 rm -rf $TMP_DIR/Settings
 rm -rf $TMP_DIR/noAnalytics
 
 # ensure dalvik-cache exists
 mkdir -p /cache/dalvik-cache
-mkdir -p /cache/dalvik-cache
 
 mkdir -p $TMP_DIR/Settings/smali
 mkdir -p $TMP_DIR/noAnalytics/smali/com/google/analytics/tracking/android
 
-echo "Extracting classes.dex from noAnalytics..." >> $LOGFILE
+echo 'Extracting classes.dex from noAnalytics...' >> $LOGFILE
 $BASE_DIR/busybox unzip $BASE_DIR/noAnalytics-dvk.jar classes.dex -d $TMP_DIR/noAnalytics >> $LOGFILE
-echo "Extracting classes.dex from Settings..." >> $LOGFILE
+echo 'Extracting classes.dex from Settings...' >> $LOGFILE
 $BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings >> $LOGFILE || $BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings >> $LOGFILE
 
-echo "Disassemble classes.dex from Settings..." >> $LOGFILE
+echo 'Disassemble classes.dex from Settings...' >> $LOGFILE
 dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/Settings/smali $TMP_DIR/Settings/classes.dex >> $LOGFILE
-echo "Disassemble classes.dex from noAnalytics..." >> $LOGFILE
+echo 'Disassemble classes.dex from noAnalytics...' >> $LOGFILE
 dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/noAnalytics/smali $TMP_DIR/noAnalytics/classes.dex >> $LOGFILE
 
-echo "Remove old Google Analytics..." >> $LOGFILE
+echo 'Remove old Google Analytics...' >> $LOGFILE
 rm -rf $TMP_DIR/Settings/smali/com/google/analytics
 rm -rf $TMP_DIR/Settings/smali/com/google/android/gms
 
-echo "Insert noAnalytics..." >> $LOGFILE
+echo 'Insert noAnalytics...' >> $LOGFILE
 cp -r $TMP_DIR/noAnalytics/smali $TMP_DIR/Settings
 
-echo "Reassembling classes.dex..." >> $LOGFILE
+echo 'Reassembling classes.dex...' >> $LOGFILE
 rm $TMP_DIR/Settings/classes.dex
 dalvikvm -Xmx256m -cp $BASE_DIR/smali-dvk.jar org.jf.smali.main  -o $TMP_DIR/Settings/classes.dex $TMP_DIR/Settings/smali >> $LOGFILE
 
-echo "Adding new classes.dex to Settings.apk..." >> $LOGFILE
+echo 'Adding new classes.dex to Settings.apk...' >> $LOGFILE
 cd $TMP_DIR/Settings
 echo classes.dex | zip -0 -@ $SETTINGS_APP >> $LOGFILE
 
-echo "Cleaning up apps..." >> $LOGFILE
+echo 'Cleaning up apps...' >> $LOGFILE
 rm /system/*app/CMAccount.apk
 rm /system/*app/CMS.apk
 rm /system/*app/LockClock.apk
@@ -83,11 +82,11 @@ rm /system/*app/VoicePlus.apk
 rm /system/*app/WhisperPush.apk
 
 
-echo "Installing self-reflasher..." >> $LOGFILE
+echo 'Installing self-reflasher...' >> $LOGFILE
 cp $BASE_DIR/20-freecyngn.sh /system/addon.d/20-freecyngn.sh
 chmod 755 /system/addon.d/20-freecyngn.sh
 
 echo >> $LOGFILE
-echo "done" >> $LOGFILE
+echo 'done' >> $LOGFILE
 
-echo "Have fun!"
+echo 'Have fun!'
