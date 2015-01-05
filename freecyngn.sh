21456cbb500a5fa3cbf077c3f37d183763552835
mar-v-in <github@rvin.mooo.com>
2015-01-05 21:16:11 +0100

    Allow second dalvik-cache location, more logs

--- freecyngn.sh
+++ freecyngn.sh
@@ -20,7 +20,7 @@ export BOOTCLASSPATH=/system/framework/core.jar:/system/framework/conscrypt.jar:
 
 if ! [ -f $BASE_DIR/busybox ] || ! [ -f $BASE_DIR/noAnalytics-dvk.jar ] || ! [ -f $BASE_DIR/baksmali-dvk.jar ] || ! [ -f $BASE_DIR/smali-dvk.jar ]
 then
-	echo "Patch framework incomplete, can't continue!" >> $LOGFILE
+	echo "!!! Patch framework incomplete, can't continue!" >> $LOGFILE
 	exit
 fi
 
@@ -33,60 +33,54 @@ fi
 
 if ! [ -f $SETTINGS_APP ]
 then
-	echo 'Did not find Settings.apk on /system, is any ROM installed?' >> $LOGFILE
+	echo '!!! Did not find Settings.apk on /system, is any ROM installed?' >> $LOGFILE
 	exit
 fi
 
-echo 'Creating directory structure...' >> $LOGFILE
-rm -rf $TMP_DIR/Settings
-rm -rf $TMP_DIR/noAnalytics
+echo '--- Creating directory structure...' >> $LOGFILE
+rm -rf $TMP_DIR/Settings $TMP_DIR/noAnalytics 2>&1 >> $LOGFILE
 
 # ensure dalvik-cache exists
-mkdir -p /cache/dalvik-cache
+mkdir -p /cache/dalvik-cache /data/dalvik-cache 2>&1 >> $LOGFILE
 
-mkdir -p $TMP_DIR/Settings/smali
-mkdir -p $TMP_DIR/noAnalytics/smali/com/google/analytics/tracking/android
+mkdir -p $TMP_DIR/Settings/smali 2>&1 >> $LOGFILE
+mkdir -p $TMP_DIR/noAnalytics/smali/com/google/analytics/tracking/android 2>&1 >> $LOGFILE
 
-echo 'Extracting classes.dex from noAnalytics...' >> $LOGFILE
-$BASE_DIR/busybox unzip $BASE_DIR/noAnalytics-dvk.jar classes.dex -d $TMP_DIR/noAnalytics >> $LOGFILE
-echo 'Extracting classes.dex from Settings...' >> $LOGFILE
-$BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings >> $LOGFILE || $BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings >> $LOGFILE
+echo '--- Extracting classes.dex from noAnalytics...' >> $LOGFILE
+$BASE_DIR/busybox unzip $BASE_DIR/noAnalytics-dvk.jar classes.dex -d $TMP_DIR/noAnalytics 2>&1 >> $LOGFILE
+echo '--- Extracting classes.dex from Settings...' >> $LOGFILE
+$BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings 2>&1 >> $LOGFILE || $BASE_DIR/busybox unzip $SETTINGS_APP classes.dex -d $TMP_DIR/Settings 2>&1 >> $LOGFILE
 
-echo 'Disassemble classes.dex from Settings...' >> $LOGFILE
-dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/Settings/smali $TMP_DIR/Settings/classes.dex >> $LOGFILE
-echo 'Disassemble classes.dex from noAnalytics...' >> $LOGFILE
-dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/noAnalytics/smali $TMP_DIR/noAnalytics/classes.dex >> $LOGFILE
+echo '--- Disassemble classes.dex from Settings...' >> $LOGFILE
+dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/Settings/smali $TMP_DIR/Settings/classes.dex 2>&1 >> $LOGFILE
+echo '--- Disassemble classes.dex from noAnalytics...' >> $LOGFILE
+dalvikvm -cp $BASE_DIR/baksmali-dvk.jar org.jf.baksmali.main -o $TMP_DIR/noAnalytics/smali $TMP_DIR/noAnalytics/classes.dex 2>&1 >> $LOGFILE
 
-echo 'Remove old Google Analytics...' >> $LOGFILE
-rm -rf $TMP_DIR/Settings/smali/com/google/analytics
-rm -rf $TMP_DIR/Settings/smali/com/google/android/gms
+echo '--- Remove old Google Analytics...' >> $LOGFILE
+rm -rf $TMP_DIR/Settings/smali/com/google/analytics $TMP_DIR/Settings/smali/com/google/android/gms 2>&1 >> $LOGFILE
 
-echo 'Insert noAnalytics...' >> $LOGFILE
-cp -r $TMP_DIR/noAnalytics/smali $TMP_DIR/Settings
+echo '--- Insert noAnalytics...' >> $LOGFILE
+cp -r $TMP_DIR/noAnalytics/smali $TMP_DIR/Settings 2>&1 >> $LOGFILE
 
-echo 'Reassembling classes.dex...' >> $LOGFILE
-rm $TMP_DIR/Settings/classes.dex
-dalvikvm -Xmx256m -cp $BASE_DIR/smali-dvk.jar org.jf.smali.main  -o $TMP_DIR/Settings/classes.dex $TMP_DIR/Settings/smali >> $LOGFILE
+echo '--- Reassembling classes.dex...' >> $LOGFILE
+rm $TMP_DIR/Settings/classes.dex 2>&1 >> $LOGFILE
+dalvikvm -Xmx256m -cp $BASE_DIR/smali-dvk.jar org.jf.smali.main  -o $TMP_DIR/Settings/classes.dex $TMP_DIR/Settings/smali 2>&1 >> $LOGFILE
 
-echo 'Adding new classes.dex to Settings.apk...' >> $LOGFILE
+echo '--- Adding new classes.dex to Settings.apk...' >> $LOGFILE
 cd $TMP_DIR/Settings
-echo classes.dex | $BASE_DIR/zip -0 -@ $SETTINGS_APP >> $LOGFILE
+echo classes.dex | $BASE_DIR/zip -0 -@ $SETTINGS_APP 2>&1 >> $LOGFILE
 
-echo 'Cleaning up apps...' >> $LOGFILE
-rm /system/*app/CMAccount.apk
-rm /system/*app/CMS.apk
-rm /system/*app/LockClock.apk
-rm /system/*app/Voice+.apk
-rm /system/*app/VoiceDialer.apk
-rm /system/*app/VoicePlus.apk
-rm /system/*app/WhisperPush.apk
+echo '--- Cleaning up apps...' >> $LOGFILE
+rm -f /system/*app/CMAccount.apk /system/*app/CMS.apk /system/*app/LockClock.apk  2>&1 >> $LOGFILE
+rm -f /system/*app/Voice+.apk /system/*app/VoiceDialer.apk /system/*app/VoicePlus.apk  2>&1 >> $LOGFILE
+rm -f /system/*app/WhisperPush.apk 2>&1 >> $LOGFILE
 
 
-echo 'Installing self-reflasher...' >> $LOGFILE
-cp $BASE_DIR/20-freecyngn.sh /system/addon.d/20-freecyngn.sh
-chmod 755 /system/addon.d/20-freecyngn.sh
+echo '--- Installing self-reflasher...' >> $LOGFILE
+cp $BASE_DIR/20-freecyngn.sh /system/addon.d/20-freecyngn.sh 2>&1 >> $LOGFILE
+chmod 755 /system/addon.d/20-freecyngn.sh 2>&1 >> $LOGFILE
 
 echo >> $LOGFILE
-echo 'done' >> $LOGFILE
+echo '--- done' >> $LOGFILE
 
 echo 'Have fun!'
